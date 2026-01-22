8855f61795a4fb430e23808250bd96e1990ad8dd
elizaburenok <elizaburenok@users.noreply.github.com>
2026-01-22 07:16:57 +0500

    Update deployment permissions in GitHub Actions and change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personalcalendar/', // GitHub repository name
+  base: '/calendar/', // GitHub repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
